a698c1ab755a33d06b8dc9f1571ca0f4416ec973
Mistra <sperectrum@gmail.com>
2021-06-22 22:28:05 +0200

    now native building works

--- src/main/java/com/example/gateway/GatewayApplication.java
+++ src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,12 +2,17 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory;
+import org.springframework.nativex.hint.TypeHint;
 
+@TypeHint(
+        types = PathRoutePredicateFactory.class,
+        typeNames =
+                "org.springframework.cloud.gateway.handler.predicate.PathRoutePredicateFactory$Config")
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 }
